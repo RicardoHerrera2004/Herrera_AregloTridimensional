3f7f6f081ed26c3dfa413e693e73d33ea4bbce08
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-30 16:46:56 -0500

    Ultimo commit: Documentacion del codigo

--- Tridimensional.c
+++ Tridimensional.c
@@ -16,28 +16,36 @@ int main()
 
     int matriz[b][c];
 
-    for (int i = 1; i < a; i++)
+    for (int i = 1; i < a; i++) // La variable de i se inicializa con 1 porque ya se va a usar una matriz fija al final llena de 1
+    // El primer for para ver cuantas matrices se va a utilizar
     {
         printf("\n Matriz %d", i);
         for (int j = 0; j < b; j++)
+        // Segundo for para ver las filas de las matrices
         {
             printf("\n");
             for (int k = 0; k < c; k++)
+            // Ultimo for para ver las columnas de las matrices
             {
                 matriz[j][k] = 0;
+                // Se llena las matrices de 0
                 printf("%d \t", matriz[j][k]);
+                // Se imprime las matrices
             }
         }
     }
 
     printf("\n Matriz %d", a);
+    // Se crea un ultimo for para la ultima matriz llena de 1
     for (int j = 0; j < b; j++)
     {
         printf("\n");
         for (int k = 0; k < c; k++)
         {
             matriz[j][k] = 1;
+            // Se llena de unos la matriz
             printf("%d \t", matriz[j][k]);
+            // Se imprime la matriz de 1 al final
         }
     }
 
