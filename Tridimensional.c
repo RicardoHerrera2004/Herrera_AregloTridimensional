6b4eac08e3807e5d19904781f237a30c9344c09e
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-30 16:35:13 -0500

    Tercer commit: Impresion de matrises tridimensionales

--- Tridimensional.c
+++ Tridimensional.c
@@ -6,24 +6,26 @@ int main()
     int a, b, c;
     // Se define las dimensionas de la matriz como enteros
 
-    printf("Ingrese las dimensiones de la matriz tridimensional (x y z) ");
     printf("Ingrese el numero de matrices que va a ingresar: ");
-    scanf("%d \n", &a);
-    printf("Ingrese el numero de filas de las matrices: ");
-    scanf("%d \n", &b);
+    scanf("%d", &a);
     printf("Ingrese el numero de columnas de las matrices: ");
-    scanf("%d \n", &c);
+    scanf("%d", &b);
+    printf("Ingrese el numero de filas de las matrices: ");
+    scanf("%d", &c);
     // Se pide al usuario las dimensiones de la matriz tridimensional
 
-    matriz[i][j];
+    int matriz[b][c];
 
     for (int i = 0; i < a; i++)
     {
+        printf ("\n Matriz %d", i);
         for (int j = 0; j < b; j++)
         {
+            printf ("\n");
             for (int k = 0; k < c; k++)
             {
-                /* code */
+                matriz[j][k]=0;               
+                printf("%d \t", matriz[j][k]);
             }
             
         }
