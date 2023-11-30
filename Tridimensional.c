a8f79971c6a6eb0c9702440f1c5577ef5eaa01ad
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-30 16:20:56 -0500

    Segundo commit: Ingreso de datos

--- Tridimensional.c
+++ Tridimensional.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main () {
+int main()
+{
 
+    int a, b, c;
+    // Se define las dimensionas de la matriz como enteros
+
+    printf("Ingrese las dimensiones de la matriz tridimensional (x y z) ");
+    printf("Ingrese el numero de matrices que va a ingresar: ");
+    scanf("%d \n", &a);
+    printf("Ingrese el numero de filas de las matrices: ");
+    scanf("%d \n", &b);
+    printf("Ingrese el numero de columnas de las matrices: ");
+    scanf("%d \n", &c);
+    // Se pide al usuario las dimensiones de la matriz tridimensional
+
+    matriz[i][j];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            for (int k = 0; k < c; k++)
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+    
 
     return 0;
 }
